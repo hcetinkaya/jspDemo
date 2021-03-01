32b5f76b0f33af7445f10d6194e47f21df3ee500
hce <hcetinkaya@hcetinkaya.de>
2021-03-01 05:34:23 +0100

    refactor

--- src/main/java/com/example/jspDemo/CalculatorServlet.java
+++ src/main/java/com/example/jspDemo/CalculatorServlet.java
@@ -10,18 +10,14 @@ import java.io.PrintWriter;
 @WebServlet("/calculate")
 public class CalculatorServlet extends HttpServlet {
     @Override
-    protected void doPost(
-            HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int a = Integer.parseInt(request.getParameter("a"));
         int b = Integer.parseInt(request.getParameter("b"));
 
-        int sum = (a+b);
+        int sum = a + b;
 
         String msg = String.format("sum of %d and %d is %d", a, b, sum);
         response.getWriter().println(msg);
-
-
     }
 
     @Override
